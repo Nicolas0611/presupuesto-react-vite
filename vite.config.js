73166aa9b2fae822a16658ef38131cff7e51b609
Nicolas Sierra <nico.sierra13@gmail.com>
2021-12-14 15:24:18 -0500

    Actualizacion vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  homepage:"/presupuesto-react-vite/",
+  base:"/presupuesto-react-vite/",
   plugins: [react()]
 })
